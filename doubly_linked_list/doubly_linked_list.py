4fb4f842e5451525ba4215a57c355f652dc86473
Christopher Riffle <cmr629@gmail.com>
2019-12-15 17:03:35 -0500

    queue MVP, link list bug fixes

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -60,7 +60,10 @@ class DoublyLinkedList:
             self.head = ListNode(value, None, self.tail)
             self.tail.prev = self.head
         else:
-            self.head.insert_before(ListNode(value))
+            node = ListNode(value, None, self.head)
+            self.head.prev = node
+            self.head = node
+            
 
         self.length += 1
 
@@ -68,7 +71,7 @@ class DoublyLinkedList:
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        value = self.head.value
+        value = self.head.value if self.head else None
 
         if self.head == self.tail:
             self.head = None
@@ -101,7 +104,7 @@ class DoublyLinkedList:
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
-        value = self.tail.value
+        value = self.tail.value if self.tail else None
 
         if self.tail == self.head:
             self.head = None
@@ -110,7 +113,9 @@ class DoublyLinkedList:
         
         elif self.tail.prev:
             self.length -= 1
-            self.tail.prev.next = None
+            self.tail = self.tail.prev
+            self.tail.next = None
+       
 
         return value
 
@@ -125,9 +130,10 @@ class DoublyLinkedList:
 
         node.delete()
 
-        node.next = head
+        node.next = self.head
         node.prev = None
         self.head.prev = node
+        self.head  = node
         
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
@@ -154,8 +160,17 @@ class DoublyLinkedList:
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev 
+
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif node == self.head:
+            self.head = self.head.next
+        elif node == self.tail:
+            self.tail = self.tail.prev
+        else:
+            node.delete()
+        
         self.length -= 1
         
     """Returns the highest value currently in the list"""
